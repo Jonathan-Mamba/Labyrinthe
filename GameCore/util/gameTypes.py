720a2a7de24581c5cd56de89d2797fc51333f9f3
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2024-11-08 18:51:28 +0100

    Modified LabGenerator, Deleted unecessary files

--- GameCore/util/gameTypes.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pygame
-
-type int_coordinate = np.ndarray[int, int] | list[int, int] | tuple[int, int]
-var = pygame.math.Vector2(1., 0.)
-var[0]
--- GameCore/util/laby_generator.py
+++ GameCore/util/laby_generator.py
@@ -29,28 +29,14 @@ def close_points(_point: point) -> list:
     ]
 
 
-class LabWalker:
-    def __init__(self, point_list: np.ndarray[int], start_point: point, func = lambda x: None) -> None:
-        self.point_list = point_list
-        self.start_point = start_point
-        self.points_dict = {tuple(start_point): True}
-        self.func = func
-
-    def generate(self, size: point,  easy_bt: bool = False) -> np.ndarray[int]:
-        return generate_lab(size, self.start_point, easy_bt, walker=self)
-
-    def at_new_cell(self, _point: point):
-        self.func(np.array(_point))
-
-
 def is_inside(_point: point, topleft: point, bottomright: point) -> bool:
     return topleft[0] <= _point[0] < bottomright[0] and topleft[1] <= _point[1] < bottomright[1]
 
 
-def next_possible_points(walker: LabWalker, topleft: point, bottomright: point, index: int = 1) -> list[int]:
+def next_possible_points(point_list: np.ndarray[int], points_dict: dict[tuple[int], bool], topleft: point, bottomright: point, index: int = 1) -> list[int]:
     possible_points = []
-    for i in close_points(walker.point_list[-index]):
-        if is_inside(i, topleft=topleft, bottomright=bottomright) and not walker.points_dict.get(tuple(i), False):
+    for i in close_points(point_list[-index]):
+        if is_inside(i, topleft, bottomright) and not points_dict.get(tuple(i), False):
             possible_points.append(i)
     return possible_points
 
@@ -58,45 +44,44 @@ def next_possible_points(walker: LabWalker, topleft: point, bottomright: point, 
 _backtrack_count = 0
 
 
-def choose_next_point(walker: LabWalker, size: list) -> point:
+def choose_next_point(point_list: np.ndarray[int], points_dict: dict[tuple[int], bool],  size: list) -> point:
     global _backtrack_count
-    next_points: list = next_possible_points(walker, [0, 0], bottomright=size)
+    next_points: list = next_possible_points(point_list, points_dict, [0, 0], size)
     backtrack = 1
 
     while next_points == []:  #si il y a aucun point disponible
         backtrack += 1
         _backtrack_count += 1
-        next_points = next_possible_points(walker, [0, 0], bottomright=size, index=backtrack)
+        next_points = next_possible_points(point_list, points_dict, [0, 0], size, backtrack)
 
     return random.choice(next_points)
 
 
-def generate_lab(size: point, start_point=None, progress_bar: bool = False, walker: LabWalker = None) -> np.ndarray[int]:
+def generate_lab(size: point, start_point=None, progress_bar: bool = False) -> np.ndarray[int]:
     """
     retourne une liste de coordonnées qui permet de générer un labyrinthe dans un repère avec [0, 0] le point le plus en haut à gauche du plan.
     """
     if start_point is None:
         start_point = [0, 0]
 
-    walker: LabWalker = LabWalker(point_list=np.array([start_point], dtype="int16"), start_point=start_point) if walker is None else walker
     AREA = size[0] * size[1]
+    point_list: np.ndarray[int] = np.zeros(size)
+    points_dict: dict[tuple[int], bool] = {}
     if progress_bar:
         with alive_progress.alive_bar(AREA, title="Génération du plan du labyrinthe") as bar:
             bar()
             for _ in range(AREA - 1):
-                next_point = choose_next_point(walker, size)
-                walker.point_list = np.append(walker.point_list, [next_point], axis=0)
-                walker.points_dict[tuple(next_point)] = True
-                walker.at_new_cell(next_point)
+                next_point = choose_next_point(point_list, points_dict, size)
+                point_list = np.append(point_list, [next_point], axis=0)
+                points_dict[tuple(next_point)] = True
                 bar()
     else:
         for _ in range(AREA - 1):
-            next_point = choose_next_point(walker, size)
-            walker.point_list = np.append(walker.point_list, [next_point], axis=0)
-            walker.points_dict[tuple(next_point)] = True
-            walker.at_new_cell(next_point)
+            next_point = choose_next_point(size)
+            point_list = np.append(point_list, [next_point], axis=0)
+            points_dict[tuple(next_point)] = True
 
-    return walker.point_list
+    return point_list
 
 
 def main() -> None:
